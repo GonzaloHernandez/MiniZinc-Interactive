e86fdc10cd4053793a3f1d0e7bdd2603d7da74ee
Gonzalo Hernandez <gonzalohernandez@udenar.edu.co>
2026-06-26 07:49:32 +1000

    Bump version to 1.0.4

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Target directory
-EXT_DIR="$HOME/.antigravity-ide/extensions/sonar-dev.minizinc-interactive-1.0.3"
+EXT_DIR="$HOME/.antigravity-ide/extensions/sonar-dev.minizinc-interactive-1.0.4"
 
 echo "=== Installing MiniZinc Interactive ==="
 echo "Target directory: $EXT_DIR"
